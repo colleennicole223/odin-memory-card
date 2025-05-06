c5df0dab4a88427d3a44d74af00bb5bc6f20ebce
colleennicole223 <153748501+colleennicole223@users.noreply.github.com>
2025-05-06 16:54:05 -0400

    Fetch API

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
 
 export default function Card({onClick, id, order}){
 
+    const [pokemon, setPokemon] = useState(null);
+
+    useEffect(() => {
+        fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+        .then((response) => response.json())            // <-- parse to JSON
+        .then((response) => setPokemon(response))       // <-- store usable data
+    })
+
+    if (!pokemon) return <p>Loading...</p>;
+
     return (
         <div onClick={() => onClick(id)} className='card'>
-            <div className='cardInlay'>ID: {id} Order: {order}</div>
+            <div className='cardInlay'>
+                ID: {id} Order: {order} Name: {pokemon.name}
+                <img className='pokeImg' src={pokemon.sprites.front_default} alt={pokemon.name} />
+            </div>
+            
+            
         </div>
     )
 }
